6a2f6b19d0a98d97c9c295bf4337e07f81a2a55d
BastianErler <erler.bastian@gmail.com>
2017-02-23 12:43:12 +0100

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,25 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
+  def setup
+    @base_title = 'BluelinerTippspiel'
+  end
+
+  test 'should get home' do
     get static_pages_home_url
     assert_response :success
+    assert_select 'title', "Home | #{@base_title}"
   end
 
-  test "should get help" do
+  test 'should get help' do
     get static_pages_help_url
     assert_response :success
+    assert_select 'title', "Hilfe | #{@base_title}"
   end
 
+  test 'should get rules' do
+    get static_pages_rules_url
+    assert_response :success
+    assert_select 'title', "Regeln | #{@base_title}"
+  end
 end
