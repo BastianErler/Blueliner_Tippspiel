02f22fa8ed01bdbfa49ef9a9fa737543ded1257e
BastianErler <erler.bastian@gmail.com>
2017-07-09 13:54:57 +0200

    m

--- app/controllers/tips_controller.rb
+++ app/controllers/tips_controller.rb
@@ -1,5 +1,5 @@
 class TipsController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit]
+  before_action :logged_in_user, only: [:edit]
   before_action :admin_user,     only: [:new, :edit]
 
   def edit
